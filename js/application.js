6c144c19a0452de8c1760457ebc87bc9455b20af
Matthias Balmer <mbalmer@smss.ch>
2013-04-03 05:46:40 +0200

    correct ecryption

--- js/application.js
+++ js/application.js
@@ -190,7 +190,7 @@ function show_events(){
     _.each(res,function(ev){
       var list_event = ev;
       list.append('<li id="'+list_event.id+'"><h3>'+list_event.name+'</h3><p>'+list_event.start_time+'</p></li>');
-      $('#'+list_event.id).click(function(ev){
+      $('#'+list_event.id).click(function(){
         selected_event = list_event;        
         $('#event_title').html(selected_event.name);        
         $.mobile.changePage('#scan', 'fade', true, true);
